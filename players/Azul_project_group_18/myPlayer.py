88133039f2e91560066d3dfc766e59fd438478e2
RUOFZHANG <ruofzhang@student.unimelb.edu.au>
2020-06-03 09:31:26 +0800

    Fix path of myPlayer

--- players/Azul_project_group_18/myPlayer.py
+++ players/Azul_project_group_18/myPlayer.py
@@ -1,8 +1,9 @@
+import sys
+sys.path.append('players/Azul_project_group_18/')
 from advance_model import *
 from players.Azul_project_group_18.mcts import monte_carlo_tree_search
 from players.Azul_project_group_18.mctsnodes import mctsNode
 
-# final version
 class myPlayer(AdvancePlayer):
     def __init__(self,_id):
         super().__init__(_id)
